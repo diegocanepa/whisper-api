f2b249ca9d843039fbfb753837a2423db4892e35
diegocanepa <diego.canepa241198@gmail.com>
2025-04-05 04:01:49 -0300

    Add entrypoint to vercel

--- app/main.py
+++ app/main.py
@@ -11,3 +11,7 @@ async def transcribe(file: UploadFile = File(...)):
     
     text = transcribe_audio(audio_path)
     return {"text": text}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
